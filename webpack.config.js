5635dca8ac6278ebc65a5dc866e0e070afc95f55
Juan Manuel Carballo <jm.carb@gmail.com>
2020-02-16 15:54:26 -0600

    Initial scaffolding with webpack and typescript

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    
+    entry: {
+        main: path.resolve(__dirname, 'src', 'main.ts')
+    },
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        contentBase: './dist'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'images',
+                    publicPath: 'assets'
+                }
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            favicon: './src/icon.png'
+        })
+    ]
+};
